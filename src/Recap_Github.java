887192a8fc1e1b7405ffc2e347ce65ae35d53a5e
hunjoonrhee <hunjoonrhee90@gmail.com>
2022-08-30 21:07:32 +0200

    Add Recap result

--- src/Recap_Github.java
+++ src/Recap_Github.java
@@ -1,5 +1,6 @@
 public class Recap_Github {
     public static void main(String[] args) {
-        System.out.println("Ich wiederhole Github.");
+        System.out.println("Ich wiederhole Github. And it works!");
+        System.out.println("Ich kann jetzt das Recap abschließen.");
     }
 }
